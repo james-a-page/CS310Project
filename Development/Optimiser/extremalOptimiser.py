c21fc38a4c1bfeff1bd4f80301fec11e4e94fba1
James Page <41538668+jamesthefishy@users.noreply.github.com>
2022-02-25 18:05:33 +0000

    .

--- Development/Optimiser/extremalOptimiser.py
+++ Development/Optimiser/extremalOptimiser.py
@@ -1,12 +1,14 @@
-from random import seed
+from random import random, seed
 from re import I
 from string import printable
+from turtle import color
 import model as pred
 import pandas as pd
 import numpy as np
 import math
 from scipy.stats import iqr
 from matplotlib import projections, pyplot as plt
+import random
 
 global allocation_to_location
 # global location_to_expected_weather
@@ -14,13 +16,16 @@ global allocation_to_location
 
 class Chromosome:
 
-    def __init__(self, size, budget):
+    def __init__(self, size, budget,genes = []):
         self.size = size
         self.budget = budget
         #Generate inital random distribution that sums to less than the budget
-        self.genes = (np.random.dirichlet(np.ones(self.size),
-                                          size=1)[0]) * self.budget
-        self.genes = [math.floor(x) for x in self.genes]
+        if genes:
+            self.genes = genes
+        else:
+            self.genes = (np.random.dirichlet(np.ones(self.size),
+                                            size=1)[0]) * self.budget
+            self.genes = [math.floor(x) for x in self.genes]
 
     def getGenes(self):
         return self.genes
@@ -29,7 +34,7 @@ class Chromosome:
 
         #Check doesn't break constraints
         if sum(self.genes) > self.budget:
-            return 0
+            return (0, 0, 0, 0)
 
         #Define Inital values
         predictionList = np.array([])
@@ -80,7 +85,8 @@ class Chromosome:
 
         #Returns Objective (F1,F2)
         # save.to_csv('../../Data/PreComputedPredictions.csv')
-        return (meanPredOutput, deviationOutput, outputIQR)
+        return (meanPredOutput, -1*np.var(predictionList), deviationOutput,
+                outputIQR)
 
 
 class Generation:
@@ -137,24 +143,103 @@ class Generation:
                     crowdingDistance[i] = min(crowdingDistance[i],
                                               distance(fit1, fit2))
 
-        plt.figure()
-        x = [x[0] for x in fitnesses]
-        y = [x[1] for x in fitnesses]
-        c = [dominatedRank[i] for i, x in enumerate(fitnesses)]
-        plt.scatter(x, y, c=c)
-        axes = plt.gca()
-        axes.set_aspect('auto')
-        plt.show()
+        # plt.figure()
+        # x = [x[0] for x in fitnesses]
+        # y = [x[1] for x in fitnesses]
+        # c = [dominatedRank[i] for i, x in enumerate(fitnesses)]
+        # plt.scatter(x, y, c=c)
+        # axes = plt.gca()
+        # axes.set_aspect('equal')
+        # plt.show()
 
         #Random Tournament Selection
         print(crowdingDistance)
         print(dominatedRank)
 
+        tournamentPool = list(range((self.popSize)))
+        selected = []
+        while tournamentPool:
+            selection1 = tournamentPool.pop(
+                random.randint(0,
+                               len(tournamentPool) - 1))
+            selection2 = tournamentPool.pop(
+                random.randint(0,
+                               len(tournamentPool) - 1))
+            if dominatedRank[selection1] < dominatedRank[selection2]:
+                selected.append(selection1)
+            elif dominatedRank[selection1] == dominatedRank[selection2]:
+                #Compare crowding distance
+                if crowdingDistance[selection1] > crowdingDistance[selection2]:
+                    selected.append(selection1)
+                else:
+                    selected.append(selection2)
+            else:
+                selected.append(selection2)
         #Reproduction
-
+        #Select two random
         #Select Random Crossover point for this generation
+        reproductionPool = list(range((self.popSize)))
+        offSpring = []
+        while reproductionPool:
+            chromosome1 = self.population[reproductionPool.pop(
+                random.randint(0,
+                               len(reproductionPool) - 1))]
+            chromosome2 = self.population[reproductionPool.pop(
+                random.randint(0,
+                               len(reproductionPool) - 1))]
+            crossoverPoint = random.randint(1, self.geneCount - 1)
 
-        #
+            offSpring.append(Chromosome(self.geneCount,self.budget,chromosome1.getGenes()[:crossoverPoint] +
+                             chromosome2.getGenes()[crossoverPoint:]))
+            offSpring.append(Chromosome(self.geneCount,self.budget,chromosome2.getGenes()[:crossoverPoint] +
+                             chromosome1.getGenes()[crossoverPoint:]))
+
+        #Mutate a random gene at chance self.mutationRate
+
+        #Combine last gen and offspring into one set, re-sort and take top half as elitist principle
+        for i, chromosome in enumerate(offSpring):
+            print(i, '/', self.popSize)
+            fitnesses.append(chromosome.fitness(self.fitnessModel))
+
+        # dominatedRank = {}
+        # crowdingDistance = {}
+
+        for i, fit1 in enumerate(fitnesses):
+            dominatedRank[i+self.popSize] = 0
+            crowdingDistance[i+self.popSize] = 100
+            for fit2 in fitnesses:
+                if (fit1 != fit2):
+                    if (dominated(fit1, fit2)):
+                        if (i in dominatedRank.keys()):
+                            dominatedRank[i+self.popSize] += 1
+                #Calculate Crowding distance
+                    crowdingDistance[i+self.popSize] = min(crowdingDistance[i+self.popSize],
+                                              distance(fit1, fit2))
+
+        plt.figure()
+        x_P = [x[0] for x in fitnesses[:self.popSize]]
+        y_P = [x[1] for x in fitnesses[:self.popSize]]
+        x_Q = [x[0] for x in fitnesses[self.popSize:]]
+        y_Q = [x[1] for x in fitnesses[self.popSize:]]
+        # c = [dominatedRank[i] for i, x in enumerate(fitnesses)]
+        plt.scatter(x_P, y_P, color='red')
+        plt.scatter(x_Q, y_Q, color='green')
+
+        axes = plt.gca()
+        axes.set_aspect('auto')
+        plt.show()
+
+        #Sort by ranking & crowding distance
+        print()
+        order = (sorted(dominatedRank.items(), key=lambda item: item[1]))
+        cutoffpoint = self.popSize
+        print(order[cutoffpoint-1],order[cutoffpoint],order[cutoffpoint+1])
+        #If DominatedRank changes at cutoff point then sort all in that dominated rank by crowding distance:
+        if(order[cutoffpoint-1][1]==order[cutoffpoint][1]):
+            pass
+        else:
+            #Split at cutoff point no sorting required
+            pass
 
         newPop = []
         return Generation(self.genNumber + 1, self.popSize,
@@ -182,8 +267,8 @@ def main():
     allocation_to_location = pd.read_csv('../../Data//locations.csv').drop(
         'capacity', axis=1)
     predictor = pred.initaliseModel()
-    chromosomeParameters = (56, 40)
-    gen0 = Generation(0, 100, chromosomeParameters, predictor)
+    chromosomeParameters = (56, 25)
+    gen0 = Generation(0, 50, chromosomeParameters, predictor)
     gen0.getOffspring()
 
 
