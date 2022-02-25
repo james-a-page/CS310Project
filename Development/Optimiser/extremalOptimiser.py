b6fcbe724799e10107a64b97596d5a01ef01a190
James Page <41538668+jamesthefishy@users.noreply.github.com>
2022-02-25 17:03:37 +0000

    crowding distacne

--- Development/Optimiser/extremalOptimiser.py
+++ Development/Optimiser/extremalOptimiser.py
@@ -1,9 +1,11 @@
 from random import seed
+from re import I
 from string import printable
 import model as pred
 import pandas as pd
 import numpy as np
 import math
+from scipy.stats import iqr
 from matplotlib import projections, pyplot as plt
 
 global allocation_to_location
@@ -30,56 +32,55 @@ class Chromosome:
             return 0
 
         #Define Inital values
-        total = 0
         predictionList = np.array([])
         # save = pd.DataFrame(data=[],columns=['BMU_ID','predOutput'])
         precomputedPred = pd.read_csv('../../Data/PreComputedPredictions.csv')
         #For each location, model the expected weather, sample that distribution, predict outputs based on that sample
         for i, count in enumerate(self.genes):
-            np.random.seed(0)
+
             if count > 0:
 
                 #Uncomment to recompute predictions at run time -- will be very slow on large runs
-                # dataset = pd.read_csv(
-                #     '../../Data/TimeSeriesOfLocations/' +
-                #     (allocation_to_location.iloc[i].BMU_ID).replace('-', '_') +
-                #     '.csv',
-                #     parse_dates=["datetime"])
-                # distributions = {}
-                # for feature in [
-                #         'temp', 'dew', 'humidity', 'precip', 'windgust',
-                #         'windspeed', 'sealevelpressure', 'cloudcover',
-                #         'visibility'
-                # ]:
-                #     #1 - Smooth Data
-                #     featureData = dataset[[feature]]
-                #     smoothedData = (featureData.ewm(alpha=0.5)).mean()
-                #     #2 - Take Mean & std of each smoothed weather feature
-                #     #3 - Sample 1000 number of data points from normal distribution defined by step 2 (for each feature)
-                #     distributions[feature] = np.random.normal(
-                #         smoothedData.mean(), smoothedData.std(), 1000)
-                # #4 - Predict output over those points.
-                # predictions = pred.predict(pd.DataFrame(distributions), model)
+                dataset = pd.read_csv(
+                    '../../Data/TimeSeriesOfLocations/' +
+                    (allocation_to_location.iloc[i].BMU_ID).replace('-', '_') +
+                    '.csv',
+                    parse_dates=["datetime"])
+                distributions = {}
+                for feature in [
+                        'temp', 'dew', 'humidity', 'precip', 'windgust',
+                        'windspeed', 'sealevelpressure', 'cloudcover',
+                        'visibility'
+                ]:
+                    #1 - Smooth Data
+                    featureData = dataset[[feature]]
+                    # smoothedData = (featureData.ewm(alpha=0.5)).mean()
+                    #2 - Take Mean & std of each smoothed weather feature
+                    #3 - Sample 1000 number of data points from normal distribution defined by step 2 (for each feature)
+                    distributions[feature] = np.random.normal(
+                        featureData.mean(), featureData.std(), 25)
+                # 4 - Predict output over those points.
+                predictions = pred.predict(pd.DataFrame(distributions), model)
                 # save = pd.concat([save,pd.DataFrame([(allocation_to_location.iloc[i].BMU_ID,x) for x in predictions],columns=['BMU_ID','predOutput'])])
 
                 #Load precomputed predictions to speed up processing.
-                predictions = ((precomputedPred.loc[precomputedPred['BMU_ID'] == allocation_to_location.iloc[i].BMU_ID]).predOutput).array
+                # predictions = ((precomputedPred.loc[precomputedPred['BMU_ID'] == allocation_to_location.iloc[i].BMU_ID]).predOutput).array
                 for j in range(count):
                     predictionList = np.append(predictionList, (predictions))
 
-
         # F1 = mean of all allocations (maxmise)
         #Total predicted output of this allocation as 'Load factor' percentage (% of total capacity allocated)
-        meanPredOutput = predictionList.mean() #total / sum(self.genes)
-        #F1.5 - Std of predictions (minimise -> * -1)
-        deviationOutput = -1 * predictionList.std()
+        meanPredOutput = predictionList.mean()  #total / sum(self.genes)
+
         # F2 = Lower quartile of all locations
-        lowerQuartOutput = np.quantile(predictionList, 0.25)
-        # F3 = Min?
-        minOutput = predictionList.min()
+        outputIQR = -1 * iqr(predictionList)
+
+        #F3 - Std of predictions (minimise -> * -1)
+        deviationOutput = -1 * predictionList.std()
+
         #Returns Objective (F1,F2)
         # save.to_csv('../../Data/PreComputedPredictions.csv')
-        return (meanPredOutput, deviationOutput,lowerQuartOutput)
+        return (meanPredOutput, deviationOutput, outputIQR)
 
 
 class Generation:
@@ -112,51 +113,66 @@ class Generation:
     #https://link.springer.com/content/pdf/10.1007%2F978-1-4614-6940-7.pdf - Elitist Non-dominated Sorting GA (NSGA-II)
     def getOffspring(self):
         fitnesses = []
-        for chromosome in self.population:
+        for i, chromosome in enumerate(self.population):
+            print(i, '/', self.popSize)
             fitnesses.append(chromosome.fitness(self.fitnessModel))
         # print(fitnesses)
 
         #Find Non-Dominated Front
         #   Rank By fronts
         #   Rank by crowding distance
-    
-        nodeDomCount = {}
-        
-        for fit1 in fitnesses:
-            nodeDomCount[fit1] = 0
+
+        dominatedRank = {}
+        crowdingDistance = {}
+
+        for i, fit1 in enumerate(fitnesses):
+            dominatedRank[i] = 0
+            crowdingDistance[i] = 100
             for fit2 in fitnesses:
-                if (fit1 == fit2):
-                    pass
-                elif (dominated(fit1, fit2)):
-                    if (fit1 in nodeDomCount.keys()):
-                        nodeDomCount[fit1] += 1
-                    
-                    # dominatedNodes.append(fit1)
-                    
+                if (fit1 != fit2):
+                    if (dominated(fit1, fit2)):
+                        if (i in dominatedRank.keys()):
+                            dominatedRank[i] += 1
+                #Calculate Crowding distance
+                    crowdingDistance[i] = min(crowdingDistance[i],
+                                              distance(fit1, fit2))
 
         plt.figure()
         x = [x[0] for x in fitnesses]
         y = [x[1] for x in fitnesses]
-        c = [nodeDomCount[x] for x in fitnesses]
-        plt.scatter(x,y,c=c)
+        c = [dominatedRank[i] for i, x in enumerate(fitnesses)]
+        plt.scatter(x, y, c=c)
+        axes = plt.gca()
+        axes.set_aspect('auto')
         plt.show()
 
         #Random Tournament Selection
+        print(crowdingDistance)
+        print(dominatedRank)
+
+        #Reproduction
+
+        #Select Random Crossover point for this generation
+
         #
 
-
-
-        
         newPop = []
         return Generation(self.genNumber + 1, self.popSize,
                           (self.geneCount, self.budget), newPop)
 
 
 def dominated(fitness_a, fitness_b):
-    if ((fitness_a[0] <= fitness_b[0]) and (fitness_a[1] < fitness_b[1]) or ((fitness_a[0] < fitness_b[0]) and (fitness_a[1] <= fitness_b[1]))):
+    if ((fitness_a[0] <= fitness_b[0]) and (fitness_a[1] < fitness_b[1]) or
+        ((fitness_a[0] < fitness_b[0]) and (fitness_a[1] <= fitness_b[1]))):
         return True
     else:
         return False
+
+
+def distance(fitness_a, fitness_b):
+    a = np.array([fitness_a[0], fitness_a[1]])
+    b = np.array([fitness_b[0], fitness_b[1]])
+    return np.linalg.norm(a - b)
 
 
 #Based upon: https://www.sciencedirect.com/science/article/pii/S0020025515007276
@@ -166,7 +182,7 @@ def main():
     allocation_to_location = pd.read_csv('../../Data//locations.csv').drop(
         'capacity', axis=1)
     predictor = pred.initaliseModel()
-    chromosomeParameters = (56, 500)
+    chromosomeParameters = (56, 40)
     gen0 = Generation(0, 100, chromosomeParameters, predictor)
     gen0.getOffspring()
 
